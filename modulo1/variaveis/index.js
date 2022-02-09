7e2a5e178f797bedf2b80a9b7322dd13143ae9ef
Andre Rudella <rudella15@gmail.com>
2022-02-08 23:24:00 -0300

    exercício feito

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,40 @@
+/* 1) exercício  
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+ R = no console.log irá aparecer 10,5
+
+ 2) let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+R console.log(10,10,10)
+
+3)
+ let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+R= let horasTrabalhadas
+let quantoRecebe*/
+
+
+
+
+
+nome = "andre"
+idade = 22
+
+
+typeof nome
+typeof idade
+
+
