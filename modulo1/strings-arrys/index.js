7212aa46c2122a8ed35286e78ac51236822fd615
Andre Rudella <rudella15@gmail.com>
2022-02-15 23:18:50 -0300

    execício

---
+++ modulo1/strings-arrys/index.js
@@ -0,0 +1,33 @@
+1 /*let array
+console.log('a. ', array)*/
+a) undefined
+
+/* array = null
+console.log('b. ', array)*/
+b) null
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+c) 11
+
+let i = 0
+console.log('d. ', array[i])
+d) como o array não foi defenido, irá dar undefined
+
+array[i+1] = 19
+console.log('e. ', array)
+e) 19
+
+const valor = array[i+6]
+console.log('f. ', valor)
+d) 6
+
+2)
+ SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+
+1) 
+const email = prompt(" por favor, digite seu email")
+const nome = prompt(" por favor, digite seu nome ")
+
+console.log(" O e-mail " + email + " foi cadastrado com sucesso. Seja bem-vinda(o) " + nome)
