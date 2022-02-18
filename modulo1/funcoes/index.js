899ee8d37a0e06bdb443165ea2daf59318d606b9
Andre Rudella <rudella15@gmail.com>
2022-02-17 23:24:09 -0300

    novo ecercício

--- modulo1/funcoes/index.js
+++ modulo1/funcoes/index.js
@@ -1,4 +1,44 @@
-function numero (x) {
+/* 1
+a) 10 e 50 respectivamente
+b) não ia aparecer mais nada no console, pois para aparecer no console é necessário o comando console.log
+
+2) 
+a) a funcao é para determinar se no texto há a palavra cenoura nele, se tiver ele resulta em True, se não tiver ele resulta em false
+b) true,true, false
+
+----
+
+1) 
+function console() { 
+
+console.log( " Me chamo André Tenho 22 anos, moro em São Paulo e sou estudante")
+
+}
+
+    console()
+
+2) 
+A) function numero ( umNumero , segundoNumero){ 
+
+    const resultado = umNumero + segundoNumero
+
+    return resultado }
+
+    numero( 3 , 5)
+
+    console.log(  numero( 3 , 5) )
+
+b) function numero ( umNumero , segundoNumero){ 
+
+    const resultado = umNumero >= segundoNumero
+
+    return resultado }
+
+    numero( 3 , 5)
+
+    console.log(  numero( 3 , 5) )
+
+c) function numero (x) {
     return (x%2) === 0
 }
 
