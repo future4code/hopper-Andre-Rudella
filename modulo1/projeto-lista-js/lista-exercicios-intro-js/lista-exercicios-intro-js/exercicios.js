23039adec8db12dd90a9139ae42949e5be105ce3
Andre Rudella <rudella15@gmail.com>
2022-02-18 23:48:07 -0300

    projeto lista

---
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/lista-exercicios-intro-js/exercicios.js
@@ -0,0 +1,134 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+  
+  let altura = Number(prompt( "digite a altura do retangulo"))
+  let largura = Number (prompt( "digite a largura do retangulo"))
+  
+  const resultado = altura * largura
+   
+  console.log(resultado)
+}
+
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+
+  let anoAtual = Number(prompt( "digite em que ano esstamos"))
+  let dataNascimeto = Number( prompt( "digite sua data de nascimento"))
+
+
+  const idade = anoAtual - dataNascimeto
+
+  console.log(idade)
+}
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+  
+return peso/(altura*altura) 
+
+
+
+}
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+const nome = prompt(`nome`)
+const idade = prompt(`idade`)
+const email = prompt(`email`)
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+
+  
+
+}
+
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  let cor1 = azul
+  let cor2 = verde
+  let cor3= vermelho
+
+  
+
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
