10baa6b9194b14459e55e5ec43005624d5b588be
Andre Rudella <rudella15@gmail.com>
2022-03-10 22:46:30 -0300

    novo exercício

---
+++ modulo1/callback/index.js
@@ -0,0 +1,9 @@
+1) 
+a) /* ele irá executar no console o mapeamento da const usuários, mostrando o índice contendo o nome e a local do array */
+
+2
+a) /* nesse caso diferente do exercicío 1, ele irá mostrar somente os nomes*/
+
+3
+a) /* ele ira mostrar no console todos os nomes diferentes de chijo. 
+
---
+++ modulo1/lacos/index.js
@@ -0,0 +1,5 @@
+let valor = 0
+for(let i = 0; i < 4; i++) {
+  valor += i
+}
+console.log(valor)
